763530ced0c9e904b801397763c1c842d507e708
izerozlu <izerozlu@gmail.com>
2022-10-10 20:48:14 +0200

    Fix authentication problem

--- middleware/authentication.global.ts
+++ middleware/authentication.global.ts
@@ -1,17 +1,22 @@
 import EAuthentication from '@/enums/authentication';
 import ERoutes from '@/enums/routes';
 
-export default defineNuxtRouteMiddleware((to, from) => {
-  const isLoggedInCookie = useCookie(EAuthentication.IS_LOGGED_IN);
+export default defineNuxtRouteMiddleware((to) => {
+  const authCookies = {
+    isLoggedIn: useCookie(EAuthentication.IS_LOGGED_IN),
+    username: useCookie(EAuthentication.USERNAME),
+  };
   const authenticationRouteName = 'authentication';
+  const isAuthenticated =
+    authCookies.isLoggedIn.value && authCookies.username.value;
 
-  if (!isLoggedInCookie.value && to.path !== ERoutes.AUTHENTICATION) {
+  if (!isAuthenticated && to.path !== ERoutes.AUTHENTICATION) {
     return navigateTo(ERoutes.AUTHENTICATION);
   }
-
+  
   if (
     to.name.toString().toLowerCase() === authenticationRouteName &&
-    isLoggedInCookie.value
+    isAuthenticated
   ) {
     return navigateTo(ERoutes.PATIENT_LIST);
   }
