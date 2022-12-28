631c73308a8dcf87bba8461431736c58cb0e901e
izerozlu <izerozlu@gmail.com>
2022-12-28 15:14:13 +0100

    Fix startTime parsing for PatientDetail page

--- pages/patient-detail.vue
+++ pages/patient-detail.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="patient-detail">
-    <div class="patient-detail__field field">
-      <span class="field__label"> {{ t('fields.fullname') }}: </span>
-      <span class="field__value">
+  <div class='patient-detail'>
+    <div class='patient-detail__field field'>
+      <span class='field__label'> {{ t('fields.fullname') }}: </span>
+      <span class='field__value'>
         {{
           patientStore.patientDetail.name +
           ' ' +
@@ -11,64 +11,64 @@
       </span>
     </div>
     <div
-      class="patient-detail__field field"
-      v-for="{ field, translationKey } of fields"
+      class='patient-detail__field field'
+      v-for='{ field, translationKey } of fields'
     >
-      <span class="field__label"> {{ t(`fields.${translationKey}`) }}: </span>
-      <span class="field__value">
+      <span class='field__label'> {{ t(`fields.${translationKey}`) }}: </span>
+      <span class='field__value'>
         {{ patientStore.patientDetail[field] }}
       </span>
     </div>
-    <div class="patient-detail__field field">
-      <span class="field__label"> {{ t('fields.diagnosis') }}: </span>
-      <span class="field__value">
+    <div class='patient-detail__field field'>
+      <span class='field__label'> {{ t('fields.diagnosis') }}: </span>
+      <span class='field__value'>
         {{ diagnosis }}
       </span>
     </div>
-    <div class="patient-detail__videos mt-12">
-      <span class="patient-detail__videos-header text-2xl font-bold block mb-4">
+    <div class='patient-detail__videos mt-12'>
+      <span class='patient-detail__videos-header text-2xl font-bold block mb-4'>
         {{ t('videos') }}
       </span>
-      <div class="patient-detail__video-list flex flex-wrap">
+      <div class='patient-detail__video-list flex flex-wrap'>
         <div
-          class="patient-detail__video video mb-12 mr-8 flex flex-col items-center w-[600px]"
-          v-for="(video, index) of patientStore.patientDetail.videos"
+          class='patient-detail__video video mb-12 mr-8 flex flex-col items-center w-[600px]'
+          v-for='(video, index) of patientStore.patientDetail.videos'
         >
           <video
-            class="video__player rounded-xl w-[600px] rounded-b-none"
-            :src="video.fileUrl"
+            class='video__player rounded-xl w-[600px] rounded-b-none'
+            :src='video.fileUrl'
             controls
-            @play="(event) => activateVideo({ video, event, refIndex: index })"
-            ref="videoRefs"
+            @play='(event) => activateVideo({ video, event, refIndex: index })'
+            ref='videoRefs'
           />
           <div
-            class="video__properties flex flex-col text-base p-3 mx-4 rounded-xl rounded-t-none w-full h-min"
+            class='video__properties flex flex-col text-base p-3 mx-4 rounded-xl rounded-t-none w-full h-min'
           >
             <span
-              class="video__title font-semibold"
+              class='video__title font-semibold'
               :class="{ 'mb-4': video.videoTagList?.length }"
             >
               {{ video.title }}
             </span>
-            <div class="video__tags flex flex-wrap">
+            <div class='video__tags flex flex-wrap'>
               <button
-                class="video__tag tag rounded-xl mr-2 mb-2 border-2 border-solid p-2 hover:bg-gray-200 active:bg-gray-300"
+                class='video__tag tag rounded-xl mr-2 mb-2 border-2 border-solid p-2 hover:bg-gray-200 active:bg-gray-300'
                 :class="{
                   'bg-american-purple text-white hover:text-black':
                     !tag.tag || !tag.endTime,
                 }"
-                type="button"
-                v-for="tag of video.videoTagList"
-                @click="(event) => activateTag({ tag, video, event })"
+                type='button'
+                v-for='tag of video.videoTagList'
+                @click='(event) => activateTag({ tag, video, event })'
               >
-                <span class="tag__name mr-2 font-bold" v-if="tag.tag">
+                <span class='tag__name mr-2 font-bold' v-if='tag.tag'>
                   {{ tag.tag }}:
                 </span>
-                <span class="tag__no-name font-bold mr-2" v-else>
+                <span class='tag__no-name font-bold mr-2' v-else>
                   {{ t('new-tag') }}:
                 </span>
-                <span class="tag__start-time mr-1">{{ tag.startTime }}</span>
-                <span class="tag__end-time" v-if="tag.endTime">
+                <span class='tag__start-time mr-1'>{{ tag.startTime }}</span>
+                <span class='tag__end-time' v-if='tag.endTime'>
                   - {{ tag.endTime }}
                 </span>
               </button>
@@ -78,16 +78,16 @@
       </div>
     </div>
     <TagRegistrationModal
-      :tag="tagRegistration.tag"
-      :video="tagRegistration.video"
-      :visible="tagRegistration.visible"
-      @cancel="cancelTagRegistration"
-      @save="saveTagRegistration"
+      :tag='tagRegistration.tag'
+      :video='tagRegistration.video'
+      :visible='tagRegistration.visible'
+      @cancel='cancelTagRegistration'
+      @save='saveTagRegistration'
     />
   </div>
 </template>
 
-<script lang="ts" setup>
+<script lang='ts' setup>
 import { useI18n } from 'vue-i18n';
 
 import ERoutes from '@/enums/routes';
@@ -101,7 +101,7 @@ import IServerResponse from '~/interfaces/server-response';
 
 const fields = [
   { field: 'birthDate', translationKey: 'birth-date' },
-  { field: 'savedDate', translationKey: 'saved-date' },
+  { field: 'savedDate', translationKey: 'saved-date' }
 ];
 
 const patientStore = usePatientStore();
@@ -112,18 +112,16 @@ const router = useRouter();
 definePageMeta({
   title: 'Patient Detail',
   layout: 'with-sidenav',
-  alias: ERoutes.PATIENT_DETAIL,
+  alias: ERoutes.PATIENT_DETAIL
 });
 
 const videoRefs = ref<HTMLVideoElement[]>();
 
-const activeVideo = ref<
-  Partial<{
-    video: IVideo;
-    element: HTMLVideoElement;
-    previousElement: HTMLVideoElement;
-  }>
->();
+const activeVideo = ref<Partial<{
+  video: IVideo;
+  element: HTMLVideoElement;
+  previousElement: HTMLVideoElement;
+}>>();
 const tagRegistration = ref<{
   tag?: ITag;
   video?: IVideo;
@@ -151,25 +149,29 @@ const newTagsOfActiveVideo = computed(() => {
 const sortVideoTags = () => {
   patientStore.patientDetail.videos?.forEach((video) => {
     video.videoTagList = video.videoTagList.sort((tagA: ITag, tagB: ITag) => {
-      const [tagAStartMinute, tagAStartSecond] = tagA.startTime.split(':');
-      const tagAValue = Number.parseInt(tagAStartMinute) * 60 + tagAStartSecond;
-      const [tagBStartMinute, tagBStartSecond] = tagB.startTime.split(':');
-      const tagBValue = Number.parseInt(tagBStartMinute) * 60 + tagBStartSecond;
+      const tagAValue = humanReadableStartTimeToSeconds(tagA.startTime);
+      const tagBValue = humanReadableStartTimeToSeconds(tagB.startTime);
 
       return tagAValue === tagBValue ? 0 : tagAValue > tagBValue ? 1 : -1;
     });
   });
 };
 
-const activateTag = ({
-  tag,
-  video,
-  event,
-}: {
-  video: IVideo;
-  tag: ITag;
-  event: MouseEvent;
-}) => {
+const humanReadableStartTimeToSeconds = (startTime: string): number => {
+  const [hours, minutes, seconds] = startTime.split(':');
+  return Number.parseInt(hours) * 3600 + Number.parseInt(minutes) * 60 + Number.parseInt(seconds);
+};
+
+const activateTag = (
+  {
+    tag,
+    video,
+    event
+  }: {
+    video: IVideo;
+    tag: ITag;
+    event: MouseEvent;
+  }) => {
   setCurrentTimeForVideo(event, tag.startTime);
   if (!tag.tag || !tag.endTime) {
     registerTag(tag, video);
@@ -181,16 +183,14 @@ const setCurrentTimeForVideo = (event: MouseEvent, startTime: string) => {
     .closest('.patient-detail__video')
     .querySelector('video');
 
-  const [minute, second] = startTime.split(':');
-  videoElement.currentTime =
-    Number.parseInt(minute) * 60 + Number.parseInt(second);
+  videoElement.currentTime = humanReadableStartTimeToSeconds(startTime);
 };
 
 const registerTag = (tag: ITag, video: IVideo) => {
   tagRegistration.value = {
     tag: { ...tag },
     video,
-    visible: true,
+    visible: true
   };
 };
 
@@ -202,7 +202,7 @@ const saveTagRegistration = (tag: ITag) => {
   handleResponse(
     $fetch<IServerResponse>(`/api/tag/update?id=${tag.id}`, {
       body: { ...tag, video: { id: tagRegistration.value.video.id } },
-      method: 'PATCH',
+      method: 'PATCH'
     }),
     {
       success: () => {
@@ -212,17 +212,17 @@ const saveTagRegistration = (tag: ITag) => {
           );
           tagRegistration.value.video.videoTagList[indexOfTag] = tag;
         }
-      },
+      }
     }
   );
   cancelTagRegistration();
 };
 
 const activateVideo = ({
-  video,
-  refIndex,
-  event,
-}: {
+                         video,
+                         refIndex,
+                         event
+                       }: {
   video: IVideo;
   refIndex: number;
   event: Event;
@@ -231,7 +231,7 @@ const activateVideo = ({
   activeVideo.value = {
     video,
     element: videoRefs.value[refIndex],
-    previousElement: activeVideo.value?.element,
+    previousElement: activeVideo.value?.element
   };
   initializeNewTagListener();
 };
@@ -257,9 +257,7 @@ const listenForNewTag = () => {
   if (!tagRegistration.value.visible) {
     const newTagToRegister = newTagsOfActiveVideo.value
       .filter((tag) => {
-        const [minutes, seconds] = tag.startTime.split(':');
-        const startTimeInSeconds =
-          Number.parseInt(minutes) * 60 + Number.parseInt(seconds);
+        const startTimeInSeconds = humanReadableStartTimeToSeconds(tag.startTime);
         return (
           !tag.checkedForRegistration &&
           activeVideo.value.element.currentTime >= startTimeInSeconds &&
@@ -287,13 +285,13 @@ onMounted(async () => {
     sortVideoTags();
   } else {
     router.push({
-      path: ERoutes.SEARCH,
+      path: ERoutes.SEARCH
     });
   }
 });
 </script>
 
-<style scoped lang="scss">
+<style scoped lang='scss'>
 .field {
   @apply text-base px-6 py-4 w-[420px];
 
@@ -311,7 +309,7 @@ onMounted(async () => {
 }
 </style>
 
-<i18n lang="yaml">
+<i18n lang='yaml'>
 tr:
   title: Hasta Detayı
   fields:
