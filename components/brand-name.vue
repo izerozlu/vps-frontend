1d2de1c2a027f820959f8e8a01d3dd8d6560eb06
dilsenyh <dilsenyildar@gmail.com>
2023-01-03 22:47:02 +0300

    logo and brand-name has been updated (brand-name will change again after meeting supervisor.)

--- components/brand-name.vue
+++ components/brand-name.vue
@@ -2,8 +2,8 @@
   <div class="flex mb-20 brand-name justify-center items-center">
     <LogoSvg class="mr-4 brand-name__logo" />
     <h1 class="font-bold text-2xl mb-0 brand-name__title">
-      <span class="text-awesome">medi</span>
-      <span class="text-american-purple">qu</span>
+      <span class="text-awesome">Vipa</span>
+      <span class="text-american-purple">Sim</span>
     </h1>
   </div>
 </template>
